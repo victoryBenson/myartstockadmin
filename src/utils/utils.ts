7c727453b43c9d5751779141b4c9a00ae35f08fe
victoryBenson <victoryBenson98@gmail.com>
2024-11-25 19:11:49 +0100

    update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-
+    
     if (token) {
       config.headers.Authorization = `Bearer ${token}`; 
     }
